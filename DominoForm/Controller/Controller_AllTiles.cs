c5e9d9965c04541ec0f7b3a5fd317f9fc6eaab1c
Ivan <Malinkipersonal@gmail.com>
2023-03-20 18:48:56 +0100

    Arreglado el bindeo de la ip

--- DominoForm/Controller/Controller_AllTiles.cs
+++ DominoForm/Controller/Controller_AllTiles.cs
@@ -55,7 +55,7 @@ namespace DominoForm.Controller
                 createServerSocket();
                 if(wsHost is not null)
                 {
-                    wsHost!.RunAsync("http://127.0.0.1:8080");
+                    wsHost!.RunAsync("http://0.0.0.0:8080");
                 }
                 else
                 {
